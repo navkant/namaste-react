4493679b02c99320b7960b73cec72eb73711e3ea
navkant <navkant.tyagi9@gmail.com>
2023-08-02 07:25:10 +0530

    ep-01 hello from react

---
+++ app.js
@@ -0,0 +1,16 @@
+const parent = React.createElement(
+    "div", 
+    {"id": "parent"}, 
+    React.createElement( 
+        "div",
+        {"id": "child"},
+        [
+         React.createElement("h1", {}, "Hello world from React!."),
+         React.createElement("h2", {}, "I am h2 tag.")
+        ]
+    )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
