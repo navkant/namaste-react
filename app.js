ef6425aad68a68a105faa98dc677815855d662e6
navkant <navkant.tyagi9@gmail.com>
2023-08-03 21:12:41 +0530

    playing with react components

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// creating react element using JSX
-// React element
-const Heading = <h1 className="head">Namaste React using.</h1>;
+// creating react element using JSX bcz using React.createElement is cumbersome and unclean
+// this gives us React element
+const Heading = <h1 className="head">Namaste React using JSX (variable).</h1>;
 
 // React Component
 // Class based component - Old
 // Function based component - NEW
 
-// React Functional component
+// React Functional components
+// title component
+const Title = () => (
+  <h1 className="head" tabIndex="5 ">
+    Namaste React using JSX.
+  </h1>
+);
 const HeadingComponent = () => {
   return (
-    <div>
-      <h1>Namaste React function component.</h1>
+    <div id="container">
+      <Title />
+      {Title()}
+      {Heading}
+      <h1>Namaste React functional component.</h1>
     </div>
   );
 };
