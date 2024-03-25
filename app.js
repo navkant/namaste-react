91e28955c8c18c099ed11d58acaf73d1878cbb83
Tyagi, Navkant <navkant.tyagi@dyson.com>
2024-03-25 15:45:55 +0530

    ep_01_part_04_nested_html_structures_using_createElement

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+]);
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
@@ -6,4 +17,4 @@ const heading = React.createElement(
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
