2510c6414338e2f7b21400e3c9e4e2218f242a15
Tyagi, Navkant <navkant.tyagi@dyson.com>
2024-03-26 00:35:05 +0530

    ep_03_part_04_using_react_components

--- app.js
+++ app.js
@@ -2,8 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React Element using JSX
-const jsxheading = <h1 className="head">Namaste React using JSX 🚀</h1>;
+const jsxheading = (
+  <div id="parent">
+    <div id="child">
+      <h1>Namaste React using JSX 🚀</h1>
+    </div>
+  </div>
+);
+
+// React components
+// class based components - Old
+// function based components - New
+
+const HeadingComponent = () => {
+  return <h1> Namaste React Function Component 🚀</h1>;
+};
+
+const Title = () => {
+  return (
+    <h1 className="head" tabIndex="5">
+      Namaste React using JSX
+    </h1>
+  );
+};
+
+const HeadingComponent1 = () => (
+  <div>
+    <Title />
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxheading);
+root.render(<HeadingComponent1 />);
