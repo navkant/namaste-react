0eb7d68ee9cc38ea6039f62f23a312e041dfb16e
Tyagi, Navkant <navkant.tyagi@dyson.com>
2024-03-25 15:28:41 +0530

    ep_01_part_03

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-
-
-const heading = React.createElement("h1", {}, "hello world from React!!");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello world from React!!"
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
