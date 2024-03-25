e143b54d04a6bb35ff3e9e04bd721e97c93e20b1
Tyagi, Navkant <navkant.tyagi@dyson.com>
2024-03-25 22:40:10 +0530

    ep_03_part02

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "this is namaste react!!"),
-    React.createElement("h2", {}, "By Navkant Tyagi"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-]);
+// React Element
 
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "hello world from React!!"
+  "Namaste React 🚀🚀"
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading);
