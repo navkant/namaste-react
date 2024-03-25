744cea99e560263e6ef5a5a63a1a3c0d893cf98e
Navkant Tyagi <navkanttyagi@Navkants-MacBook-Air.local>
2024-03-25 19:18:33 +0530

    ep_02_part_02_parcel_modifications

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
+    React.createElement("h1", {}, "this is namaste react!!"),
+    React.createElement("h2", {}, "By Navkant Tyagi"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am an h1 tag"),
